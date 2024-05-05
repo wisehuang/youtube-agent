280b145ca95f1c2b840feb1e03812a45631de43f
Jonathan Huang <wisehuang@gmail.com>
2024-05-05 16:10:11 +0800

    Add API token to protect API

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod agent;
 mod youtube;
 
+use std::convert::Infallible;
 use async_openai::Client;
 use async_openai::config::OpenAIConfig;
-use rocket::{catch, catchers, get, Request, routes, State};
+use rocket::{catch, catchers, get, Request, request, routes, State};
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome};
 use rocket::serde::Serialize;
 use shuttle_runtime::__internals::Context;
 use shuttle_runtime::__internals::tracing_subscriber::fmt::format::Json;
@@ -29,6 +32,16 @@ pub(crate) struct Agent {
 
 struct SecretState {
     openai_api_key: String,
+    api_key: String,
+}
+
+#[derive(Debug)]
+struct ApiKey<'r>(&'r str);
+
+#[derive(Debug)]
+enum ApiKeyError {
+    Missing,
+    Invalid,
 }
 
 #[derive(Serialize)]
@@ -38,31 +51,43 @@ struct ErrorResponse {
     success: bool,
 }
 
+#[catch(400)]
+fn bad_request() -> String {
+    "Sorry, the request is invalid.".to_string()
+}
+
+#[catch(401)]
+fn unauthorized() -> String {
+    "Sorry, you are not authorized to access this resource.".to_string()
+}
+
 #[catch(404)]
 fn not_found(req: &Request) -> String {
     format!("Sorry, '{}' is not a valid path.", req.uri())
 }
 
 #[catch(500)]
-fn internal_error(req: &Request) -> String {
+fn internal_error() -> String {
     "Sorry, There is an internal server error".to_string()
 }
 
 #[get("/")]
-fn index() -> &'static str {
+fn index(key: ApiKey<'_>) -> &'static str {
     "Hello, world!"
 }
 
 #[get("/youtube/<video_id>")]
-async fn get_youtube_summary(state: &State<SecretState>, video_id: &str,) -> String {
+async fn get_youtube_summary(state: &State<SecretState>, video_id: &str, key: ApiKey<'_>) -> String {
     youtube::summarize_video(video_id, state.openai_api_key.as_str()).await
 }
 
 #[shuttle_runtime::main]
 async fn main(#[shuttle_runtime::Secrets] _secrets: SecretStore) -> shuttle_rocket::ShuttleRocket {
-    let openai_api_key = _secrets.get("OPENAI_API_KEY").context("OPENAI_API_KEY was not found")?;
+    let openai_api_key = _secrets.get("OPENAI_API_KEY").context("OPENAI_API_KEY was not found").unwrap();
+    let api_key = _secrets.get("API_TOKEN").context("API_TOKEN was not found").unwrap();
     let secret_state = SecretState {
         openai_api_key,
+        api_key,
     };
 
     let rocket = rocket::build()
@@ -70,4 +95,23 @@ async fn main(#[shuttle_runtime::Secrets] _secrets: SecretStore) -> shuttle_rock
         .mount("/", routes![index, get_youtube_summary]).manage(secret_state);
 
     Ok(rocket.into())
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for ApiKey<'r> {
+    type Error = ApiKeyError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let secret_state = req.rocket().state::<SecretState>().unwrap();
+
+        fn is_valid(key: &str, api_key: &str) -> bool {
+            key == api_key
+        }
+
+        match req.headers().get_one("Authorization") {
+            None => Outcome::Error((Status::Unauthorized, ApiKeyError::Missing)),
+            Some(key) if is_valid(key, &secret_state.api_key) => Outcome::Success(ApiKey(key)),
+            Some(_) => Outcome::Error((Status::Unauthorized, ApiKeyError::Invalid)),
+        }
+    }
 }
